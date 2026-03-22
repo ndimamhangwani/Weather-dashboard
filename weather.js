496ad8e810f42a2f4fcf1765ed96fe49ef16b66f
Ndima._dev <ndima.mhangwani@techbridlefoundation.org>
2026-03-22 13:40:21 +0200

    added the selecting all DOM elements from html

---
+++ weather.js
@@ -0,0 +1,10 @@
+//Step 1: Select all the DOM elements from html
+const form = document.querySelector('#search-form');
+const input = document.querySelector('#city-input');
+const statusDiv = document.querySelector('#status');
+const errorDiv = document.querySelector('#error-msg');
+const weatherCard = document.querySelector('#weather-card');
+const cityNameEl = document.querySelector('#city-name');
+const tempEl = document.querySelector('#temp');
+const windEl = document.querySelector('#wind');
+const conditionEl = document.querySelector('#condition');
